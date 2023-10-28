c9be28872fb156f325445a691a107e320386e96b
andrealicheri <andrealicheripakunza@gmail.com>
2023-10-28 12:55:05 +0200

    Documented functions

--- __main__.py
+++ __main__.py
@@ -3,7 +3,9 @@ import os
 from dotenv import load_dotenv
 
 class astraClass:
+    """Astra driver wrapper for the Document API. Provides you with high level functions to interact with the Datastax API's HTTP verbs."""
     def get(path: str):
+        """Interface for the GET verb, used to fetch data from a collection or a document."""
         url = f'https://{os.environ.get("ASTRA_DB_ID")}-{os.environ.get("ASTRA_DB_REGION")}.apps.astra.datastax.com/api/rest/v2/namespaces/{os.environ.get("ASTRA_DB_KEYSPACE")}/collections/{path}'
 
         headers = {
@@ -17,6 +19,7 @@ class astraClass:
 
         
     def post(path: str, data: dict):
+        """Interface for the POST verb, used to write data to a collection or a document."""
         url = f'https://{os.environ.get("ASTRA_DB_ID")}-{os.environ.get("ASTRA_DB_REGION")}.apps.astra.datastax.com/api/rest/v2/namespaces/{os.environ.get("ASTRA_DB_KEYSPACE")}/collections/{path}'
 
         headers = {
@@ -29,6 +32,7 @@ class astraClass:
         return str(response.json())
     
     def delete(path: str):
+        """Interface for the DELETE verb, used to delete data from a collection or a document."""
         url = f'https://{os.environ.get("ASTRA_DB_ID")}-{os.environ.get("ASTRA_DB_REGION")}.apps.astra.datastax.com/api/rest/v2/namespaces/{os.environ.get("ASTRA_DB_KEYSPACE")}/collections/{path}'
 
         headers = {
@@ -42,6 +46,7 @@ class astraClass:
         return str(response.json())
         
     def put(path: str, data: dict):
+        """Interface for the PUT verb, used to modify data of a collection or a document."""
         url = f'https://{os.environ.get("ASTRA_DB_ID")}-{os.environ.get("ASTRA_DB_REGION")}.apps.astra.datastax.com/api/rest/v2/namespaces/{os.environ.get("ASTRA_DB_KEYSPACE")}/collections/{path}'
 
         headers = {
@@ -54,6 +59,7 @@ class astraClass:
         return str(response.json())
     
     def init(env: str = ".env"):
+        """Loads envoirnement variables from your envoirnement file. The default path is .env"""
         load_dotenv(env)
 
 astra = astraClass
